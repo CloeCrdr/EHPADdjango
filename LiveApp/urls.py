6ed9a364f0ec49d5eb37d23c6e0c183392602a49
Cloé Cordeiro <73696923+CloeCrdr@users.noreply.github.com>
2023-06-27 11:48:24 +0200

    install environment and check settings

---
+++ LiveApp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
+app_name = 'LiveApp'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+]
--- LiveApp/views.py
+++ LiveApp/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404, redirect
+from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
+def home(request):
+    return render(request, "LiveApp/home.html")
