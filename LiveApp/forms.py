1fa0f438ebeadedfef1509c523761b2a0d9b4438
Cloé Cordeiro <73696923+CloeCrdr@users.noreply.github.com>
2023-06-28 15:50:10 +0200

    translate variables and add admin link to footer image

--- LiveApp/forms.py
+++ LiveApp/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import Contact
+from django.utils.translation import gettext_lazy as _
+
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['firstname','lastname','email','subject',"message"]
+        labels = {
+            'firstname': _('Prénom'),
+            'lastname': _('Nom'),
+            'email': _('Adresse e-mail'),
+            'subject': _('Sujet'),
+            'message': _('Message'),
+        }
